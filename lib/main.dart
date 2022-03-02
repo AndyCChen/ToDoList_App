26b63923b4350c3096ebf601078fb13d9d2ba4c1
Andy Chen <acchen@cpp.edu>
2022-03-02 12:04:25 -0800

    added fonts

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,57 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> monthTitle = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'MedievalSharp'),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromRGBO(104, 146, 255, 24),
+          centerTitle: true,
+          title: Text(monthTitle[2])
+        ),
+
+        backgroundColor: const Color.fromRGBO(68, 68, 68, 1),
+
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+          children: const <Widget>[
+            Text('Su', style: TextStyle(color: Colors.white),),
+            Text('Mo', style: TextStyle(color: Colors.white),),
+            Text('Tu', style: TextStyle(color: Colors.white),),
+            Text('We', style: TextStyle(color: Colors.white),),
+            Text('Th', style: TextStyle(color: Colors.white),),
+            Text('Fr', style: TextStyle(color: Colors.white),),
+            Text('Sa', style: TextStyle(color: Colors.white),),
+          ],
+        )
+
+      )
+    );
   }
 }
