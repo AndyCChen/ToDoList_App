bdc8f911a2a60560bcb6f4b0eac38e78998c070d
AndyCChen <acchen@cpp.edu>
2022-05-10 12:38:46 -0700

    started adding stay logged in functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'login.dart';
 
@@ -10,6 +11,15 @@ void main() async{
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  void checkLoginStatus() {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      print('logged');
+    } else {
+      print('not logged in');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
